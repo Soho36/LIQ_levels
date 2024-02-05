a8ec6296c32bbd8f2727e63cda8d4832f05fd5e4
Soho36 <vladimir.sehirev@gmail.com>
2024-02-06 00:39:04 +0200

    working ver

--- Pattern_rec.py
+++ Pattern_rec.py
@@ -4,7 +4,7 @@ import glob
 import matplotlib.pyplot as plt
 from matplotlib.dates import date2num
 
-# Beginning of the part for merged files
+# Мerged files part
 folder_path = 'TXT'
 file_pattern = f'{folder_path}/*.txt'
 file_list = glob.glob(file_pattern)
@@ -14,25 +14,27 @@ print('Files list: ', file_list)
 data_frames = [pd.read_csv(file).assign(Filename=file) for file in file_list]
 print('Dataframes from each csv: ', data_frames)
 
-# Writing dataframe to each CSV
-for file, df in zip(file_list, data_frames):
+
+for file, df in zip(file_list, data_frames):       # Writing dataframe to each CSV
     df.to_csv(file, index=False)
 
 merged_df = pd.concat(data_frames, ignore_index=True)
 print('Merged dataframes: ', merged_df)
 
-# Writing merged CSV
-merged_df.to_csv(f'TXT/merged_data.csv', index=False)
+
+merged_df.to_csv(f'TXT/merged_data.csv', index=False)  # Writing merged CSV
 
 # End of the part for merged files
 
-file_path = 'TXT/merged_data.csv'
+# file_path = 'TXT/merged_data.csv'
 # file_path = 'TXT/exel.txt'
 # file_path = 'TXT/spr.txt'
 # file_path = 'TXT/zim.txt'
 # file_path = 'TXT/extr.txt'
 # file_path = 'TXT/aehr.txt'
 # file_path = 'TXT/neog.txt'
+# file_path = 'TXT/tsla_D1.txt'
+file_path = 'TXT/neog_D1.txt'
 
 
 columns_to_parce = ['Date', 'Open', 'High', 'Low', 'Close', 'Filename']
@@ -46,7 +48,16 @@ numeric_dates = date2num(df.index)
 
 
 # PATTERN TYPE
-signal = talib.CDL3INSIDE(df['Open'], df['High'], df['Low'], df['Close'])
+
+df_pattern = pd.read_csv('Ta-lib patterns.csv')  # Reading Pattern codes from CSV
+
+idx = 2     # Choose the index of pattern here (from Ta-lib patterns.csv)
+pattern_code = df_pattern['PatternCode'].iloc[idx]
+pattern_name = df_pattern['PatternName'].iloc[idx]
+print('Current Pattern is: ', pattern_code, pattern_name)
+
+pattern_function = getattr(talib, pattern_code)
+signal = pattern_function(df['Open'], df['High'], df['Low'], df['Close'])
 
 
 # Print the signals
@@ -67,10 +78,9 @@ plt.legend()
 
 # Converting numeric dates back to daytime in order to print a date Signal occurred
 date_time_dates = pd.to_datetime(numeric_dates, unit='D')
-print('date_time_dates: ', date_time_dates)
 
 
-# Highlight signals
+# Highlight signals on the chart
 for i, s in enumerate(signal):
     if s == 100:
         signal_date = date_time_dates[i].strftime("%d-%m-%Y-%H-%M")
