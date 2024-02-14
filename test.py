edc627f596607df2f3aa6325dca93928a42b9fc2
Soho36 <vladimir.sehirev@gmail.com>
2024-02-15 00:42:12 +0200

    working candlestick

--- test.py
+++ test.py
@@ -1,18 +1,8 @@
-import pandas as pd
-
-# Sample DataFrame with 'Date' and 'Time' columns
-data = {'Date': ['2023-06-23', '2023-06-24', '2023-06-25'],
-        'Time': ['00:00:00', '12:00:00', '23:59:59']}
-df = pd.DataFrame(data)
-
-# Concatenate 'Date' and 'Time' columns into a single string
-datetime_str = df['Date'] + ' ' + df['Time']
-
-# Convert the concatenated string to datetime format and assign it to a new column 'Datetime'
-df['Datetime'] = pd.to_datetime(datetime_str)
-
-first_row = df.iloc[1]
-print(first_row)
-
-# Display the DataFrame with the new 'Datetime' column
-print(df)
+if signals is not None and not signals.empty:
+    # Convert signals to NaN where there are no signals
+    signals_with_nan = signals.where(signals != 0, np.nan)  # replace values where the condition is False
+    # Add the signals as a subplot
+    add_plots.append(mpf.make_addplot(signals_with_nan, type='scatter', markersize=1000, marker='+', panel=0))
+# Plot candlestick chart with additional plot
+mpf.plot(df, type='candle', figsize=(15, 8), title=f'{file_path}'.upper(), ylabel='Price', addplot=add_plots)
+print('Signals are plotted to candlestick chart')
