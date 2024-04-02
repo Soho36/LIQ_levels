eb1ca08721d676aa8c830abc660906ce09111c21
Soho36 <vladimir.sehirev@gmail.com>
2024-04-02 23:01:55 +0300

    price levels with addplot good

--- SR_levels.py
+++ SR_levels.py
@@ -13,7 +13,7 @@ history_file_path = 'History_data/MT5/BTCUSD_M5_today.csv'
 
 def get_stock_price(sym):
     if not use_csv_or_yf:
-        df = yf.download(sym, start='2024-03-26', end='2024-03-29', interval='15m', progress=False)
+        df = yf.download(sym, start='2024-03-20', end='2024-03-21', interval='5m', progress=False)
         df.index = pd.to_datetime(df.index)
         print(df)
         return df
@@ -36,48 +36,59 @@ def get_stock_price(sym):
 dataframe = get_stock_price(symbol)
 
 
-def find_levels(dataframe):
+def find_levels(df):
     levels_startpoints = []
     levels_endpoints = []
     support_levels = []
     resistance_levels = []
     signals_list = []
+    signals_list.insert(0, None)
+    signals_list.insert(1, None)
 
     # Support levels
-    for i in range(2, len(dataframe) - 2):
-        signals_list.append(None)
-        if (dataframe['Low'][i] < dataframe['Low'][i-1]) and \
-           (dataframe['Low'][i] < dataframe['Low'][i+1]) and \
-           (dataframe['Low'][i+1] < dataframe['Low'][i+2]) and \
-           (dataframe['Low'][i-1] < dataframe['Low'][i-2]):
-            datetime_1 = dataframe.index[i]
-            price_level_1 = dataframe['Low'][i]
-            datetime_2 = dataframe.index[-1]
-            price_level_2 = dataframe['Low'][i]
+    for i in range(2, len(df) - 2):
+        if (df['Low'][i] < df['Low'][i - 1]) and \
+           (df['Low'][i] < df['Low'][i + 1]) and \
+           (df['Low'][i + 1] < df['Low'][i + 2]) and \
+           (df['Low'][i - 1] < df['Low'][i - 2]):
+            datetime_1 = df.index[i]
+            price_level_1 = df['Low'][i]
+            datetime_2 = df.index[-1]
+            price_level_2 = df['Low'][i]
 
-            if not is_near_level(price_level_1, levels_startpoints, dataframe):
+            if not is_near_level(price_level_1, levels_startpoints, df):
                 levels_startpoints.append((datetime_1, price_level_1))
                 levels_endpoints.append((datetime_2, price_level_2))
                 support_levels.append((datetime_1, price_level_1))
                 signals_list.append(100)
+            else:
+                signals_list.append(None)
 
     # Resistance levels
-        elif ((dataframe['High'][i] > dataframe['High'][i-1]) and
-              (dataframe['High'][i] > dataframe['High'][i+1]) and
-              (dataframe['High'][i+1] > dataframe['High'][i+2]) and
-              (dataframe['High'][i-1] > dataframe['High'][i-2])):
-            datetime_1 = dataframe.index[i]
-            price_level_1 = dataframe['High'][i]
-            datetime_2 = dataframe.index[-1]
-            price_level_2 = dataframe['High'][i]
+        elif ((df['High'][i] > df['High'][i - 1]) and
+              (df['High'][i] > df['High'][i + 1]) and
+              (df['High'][i + 1] > df['High'][i + 2]) and
+              (df['High'][i - 1] > df['High'][i - 2])):
+            datetime_1 = df.index[i]
+            price_level_1 = df['High'][i]
+            datetime_2 = df.index[-1]
+            price_level_2 = df['High'][i]
 
-            if not is_near_level(price_level_1, levels_startpoints, dataframe):
+            if not is_near_level(price_level_1, levels_startpoints, df):
                 levels_startpoints.append((datetime_1, price_level_1))
                 levels_endpoints.append((datetime_2, price_level_2))
                 resistance_levels.append((datetime_1, price_level_1))
                 signals_list.append(-100)
+            else:
+                signals_list.append(None)
 
-    level_reject_signals_series = pd.Series(signals_list)[:-1]
+        else:
+            signals_list.append(None)
+
+    signals_list.extend([None, None])   # Appending two elements to the end, to match Dataframe length
+    # print(signals_list)
+    print('Signals list length: ', len(signals_list))
+    level_reject_signals_series = pd.Series(signals_list)
 
     return (levels_startpoints, levels_endpoints, support_levels, resistance_levels,
             signals_list, level_reject_signals_series)
@@ -91,10 +102,7 @@ def is_near_level(value, levels, df):
 (levels_startpoints_to_chart, levels_endpoints_to_chart, support_levels_to, resistance_levels_to, signals_list_to,
  level_reject_signals_series_to) = find_levels(dataframe)
 
-print(levels_startpoints_to_chart)
-print('Support levels list: \n', support_levels_to)
-print('Resistance levels list: \n', resistance_levels_to)
-print('Signals series: \n', level_reject_signals_series_to)
+print('Signals series length: ', len(level_reject_signals_series_to))
 
 levels_points = [[a, b] for a, b in zip(levels_startpoints_to_chart, levels_endpoints_to_chart)]
 
